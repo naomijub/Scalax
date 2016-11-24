e7db2791da9762df08dc6a2283c7adf120acade0
naomijub <jnboeira@outlook.com>
2016-11-24 16:26:14 -0300

    Added UI

---
+++ PlayGUI/src/App.scala
@@ -0,0 +1,11 @@
+import scala.swing._
+/**
+  * Created by jnaomi on 24/11/16.
+  */
+object App {
+  def main(args: Array[String]): Unit ={
+    val ui = new UI
+    ui.visible = true
+
+  }
+}
---
+++ PlayGUI/src/UI.scala
@@ -0,0 +1,9 @@
+import scala.swing._
+/**
+  * Created by jnaomi on 24/11/16.
+  */
+class UI extends MainFrame{
+  title = "Game Title"
+  preferredSize = new Dimension(640, 480)
+
+}
