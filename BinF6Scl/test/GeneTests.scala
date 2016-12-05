719bc7bd7c3e3128215479834cc6ff800cdbe2b0
naomijub <jnboeira@outlook.com>
2016-12-05 00:21:56 -0300

    Base Project and Gene Test

---
+++ BinF6Scl/test/GeneTests.scala
@@ -0,0 +1,15 @@
+import org.scalatest.BeforeAndAfter
+import org.scalatest.FunSuite
+
+/**
+  * Created by jnaomi on 05/12/16.
+  */
+class GeneTests extends FunSuite with BeforeAndAfter {
+  var gene: Gene = _
+
+  before {
+    gene = new Gene
+  }
+
+  
+}
